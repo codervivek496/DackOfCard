6b69da79512409d6e82b336f729d54007a2cbc1c
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-23 04:24:21 -0700

    Deck of cards

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ObjectOrientedPrograms
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            DeckOfCards deckOfCards = new DeckOfCards();
+            deckOfCards.Shuffle();
+            deckOfCards.Display();
+        }
+    }
+}
